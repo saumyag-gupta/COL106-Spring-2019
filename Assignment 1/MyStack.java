7eeb1182b98dd167a0004c3374f4610a4e996243
saumyag-gupta <saumyag.gupta@gmail.com>
2019-01-17 19:45:17 +0530

    Assignment 1 commit 1

---
+++ Assignment 1/MyStack.java
@@ -0,0 +1,31 @@
+
+
+public class MyStack<E>{
+	
+	public void push(E item) {
+		
+	}
+	
+	public E pop(){
+		
+	} 
+	
+	public E peek() {
+		
+	}
+	
+	public bool empty(){
+		
+	}
+}
+
+class node<E>{
+	E data;
+	node next;
+	
+	node()
+}
+
+class LL<E>{
+	
+}
